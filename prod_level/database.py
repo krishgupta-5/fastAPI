85c541e06c4d09b713bc44c1f994096a5bf6d90b
Krish Gupta <krishgupta0072@gmail.com>
2026-05-03 22:26:51 +0530

    added

--- prod_level/database.py
+++ prod_level/database.py
@@ -1,9 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://<username>/<password>@ip_address/hostname/database_name"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
