30e6c0d4bfeec06954499ad5a4f1ad81b67bed4f
Krish Gupta <krishgupta0072@gmail.com>
2026-05-03 22:42:36 +0530

    load env in main.py

--- prod_level/main.py
+++ prod_level/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from prod_level.database import engine, Base
 from prod_level.routes import users, posts, auth
+from dotenv import load_dotenv
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
-
+load_dotenv()   
 app = FastAPI()
 
 app.include_router(users.router)
